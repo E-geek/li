d326f83abd34c6a7ff8b1fc2c8f51510207e895b
Louter <luther@trember.com>
2024-06-10 04:51:36 +0200

    Fix for correct work build data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from '@/db/database.module';
 import { vacancyProviders } from '@/db/vacancy.providers';
 import { RunModule } from '@/app/run/run.module';
-import { RunService } from "@/app/run/run.service";
 import {jobProviders} from "@/db/job.providers";
 
 @Module({
   imports: [ DatabaseModule, RunModule ],
   controllers: [ AppController ],
-  providers: [ ...vacancyProviders, ...jobProviders, AppService, RunService ],
+  providers: [ ...vacancyProviders, ...jobProviders, AppService ],
 })
 export class AppModule {}
